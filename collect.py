357e8899c472ea7e35a6d0e8367eeb2e8b5b6166
anacartola <anacarolina.cartola@gmail.com>
2024-03-26 10:16:42 -0300

    Update collect.py

--- collect.py
+++ collect.py
@@ -3,7 +3,26 @@
 print("Hello")
 # %%
 import requests
-url = 'https://www.google.com'
-resp = requests.get(url)
+url = 'https://www.residentevildatabase.com/personagens/alexander-kozachenko/'
+headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'referer':'https://www.residentevildatabase.com/personagens/',
+    'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+}
+resp = requests.get(url, headers=headers)
 
+# %%
+resp.status_code
+# %%
+resp.text
 # %%
