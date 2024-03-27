0e019dba620150c490ef4c765ae6328c384e2943
anacartola <anacarolina.cartola@gmail.com>
2024-03-27 14:59:18 -0300

    Update Bloco de Estudos 1

--- collect.py
+++ collect.py
@@ -3,7 +3,9 @@
 print("Hello")
 # %%
 import requests
-url = 'https://www.residentevildatabase.com/personagens/alexander-kozachenko/'
+from bs4 import BeautifulSoup
+
+url = 'https://www.residentevildatabase.com/personagens/ada-wong/'
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
@@ -24,5 +26,25 @@ resp = requests.get(url, headers=headers)
 # %%
 resp.status_code
 # %%
-resp.text
+soup = BeautifulSoup(resp.text)
+soup
 # %%
+div_page = soup.find("div", class_="td-page-content")
+div_page
+# %%
+paragrafo = div_page.find_all("p")[1]
+paragrafo
+# %%
+ems= paragrafo.find_all("em")
+ems
+# %%
+ems[0].text
+# %%
+data = {}
+for i in ems:
+    chave, valor = i.text.split(":")
+    chave = chave.strip(" ")
+    data[chave] = valor.strip(" ")
+data
+# %%
+data["Tipo sanguíneo"]
